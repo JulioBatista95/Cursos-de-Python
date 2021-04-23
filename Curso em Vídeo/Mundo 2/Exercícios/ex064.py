921606aad9c3ae2d9c3ea2dd7734fb61e25c76fe
JulioBatista30 <75815919+JulioBatista95@users.noreply.github.com>
2021-04-23 16:05:27 -0300

    Exercício 64 Alterado

--- Curso em Vídeo/Mundo 2/Exercícios/ex064.py
+++ Curso em Vídeo/Mundo 2/Exercícios/ex064.py
@@ -1,10 +1,14 @@
 n1 = 0
 cont = 0
-total = 1
+total = 0
+#Dica: use n1 = cont = total = 0 para diminuir as linhas
+n1 = int(input('Digite um número (Digite 999 para interromper): '))
 while n1 != 999:
-    n1 = int(input('Digite um número: '))
     cont += n1
-    total *= cont
-print(cont - 999)
-print(total)
+    #cont += n1 soma todos os números digitados
+    total += 1
+    #total += 1 faz a contagem de números digitados
+    n1 = int(input('Digite um número (Digite 999 para interromper): '))
+print('A soma dos números digitados é {}'.format(cont))
+print('O total de números digitados foi {}'.format(total))
 print('Fim')
