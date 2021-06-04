ed41057ef199a76b180997a2ac38840e33bd4cb2
JulioBatista30 <75815919+JulioBatista95@users.noreply.github.com>
2021-06-04 15:32:29 -0300

    Exercício 74 - Tuplas

---
+++ Curso em Vídeo/Mundo 3/Exercícios/ex074.py
@@ -0,0 +1,6 @@
+from random import randint
+n = (randint(1, 10), randint(1, 10), randint(1, 10),
+     randint(1, 10), randint(1, 10))
+print(f'Os números sorteados foram {n}')
+print(f'O maior valor foi {max(n)}')
+print(f'O menor valor foi {min(n)}')
